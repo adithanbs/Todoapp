298408c1f648de5424aecdf4d13a15d5da23206d
adithanbs <MY_NAME@example.com>
2021-05-22 16:16:18 +0530

    Todo-done

--- todo-app/src/components/TodoForm.js
+++ todo-app/src/components/TodoForm.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-function Todoform(props) {
-  const [input, setInput] = useState("");
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
 
+const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus()
+  })
   
   const handleChange = e => {
 setInput(e.target.value);
@@ -20,18 +25,40 @@ setInput(e.target.value);
 
   return (
     <form className="todo-form" onSubmit={handleSubmit}>
+    {props.edit ? (
+     <>
       <input
-        type="text"
-        placeholder="Add a todo"
+        placeholder="Update your item"
         value={input}
         name="text"
-        className="todo-input"
+        className="todo-input edit"
         onChange={handleChange}
+        ref={inputRef}
       />
+      
+      <button onClick={handleSubmit} className="todo-button edit">Update</button>
+    </>
 
-      <button className="todo-button">Add</button>
+    ) : (
+
+      <>
+          <input
+            placeholder='Add a todo'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+          <button onClick={handleSubmit} className='todo-button'>
+            Add todo
+          </button>
+        </>
+
+    )}
     </form>
+    
   );
 }
 
-export default Todoform;
+export default TodoForm;
--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,5 +1,5 @@
 import React,{ useState } from 'react';
-import Todoform from './TodoForm';
+import TodoForm from './TodoForm';
 import Todo from "./Todo"
 
 function TodoList() {
@@ -14,8 +14,25 @@ function TodoList() {
 const newTodos = [todo, ...todos];
 
 setTodos(newTodos);
-// console.log(todo,...todos);
+ console.log(...todos);
  };
+
+const updateTodo = (todoId,newValue) => {
+    if(!newValue.text || /^\s*$/.test(newValue.text)) {
+        return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+}
+
+
+
+
+ const removeTodo = id => {
+     const removeArr = [...todos].filter(todo => todo.id !== id)
+     setTodos(removeArr);
+ }
+
 
  const completeTodo = id => {
      let updatedTodos = todos.map(todo => {
@@ -29,12 +46,17 @@ setTodos(newTodos);
  }
 
  return (
-        <div>
+        <>
             <h1> what's the plan for Today? </h1>
-            <Todoform onSubmit={addTodo} />
-            <Todo todos={todos} completeTodo = {completeTodo} />
+            <TodoForm onSubmit={addTodo} />
+            <Todo 
+            todos={todos} 
+            completeTodo = {completeTodo} 
+            removeTodo = {removeTodo}
+            updateTodo = {updateTodo}
+            />
 
-        </div>
+        </>
     )
 }
 
